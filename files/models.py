3ff581241db59d690ac731fc434f5fcd32843ea2
Vinayak Upadhyay <vinayakupadhyay42@gmail.com>
2025-04-20 00:46:49 +0530

    added indexes

--- files/models.py
+++ files/models.py
@@ -8,25 +8,6 @@ def file_upload_path(instance, filename):
     filename = f"{uuid.uuid4()}.{ext}"
     return os.path.join('uploads', filename)
 
-class File(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    file = models.FileField(upload_to=file_upload_path)
-    original_filename = models.CharField(max_length=255)
-    file_type = models.CharField(max_length=100)
-    size = models.BigIntegerField()
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        ordering = ['-uploaded_at']
-    
-    def __str__(self):
-        return self.original_filename
-
-def file_upload_path(instance, filename):
-    ext = filename.split('.')[-1]
-    filename = f"{uuid.uuid4()}.{ext}"
-    return os.path.join('uploads', filename)
-
 class StoredFile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     file = models.FileField(upload_to=file_upload_path)
@@ -40,7 +21,7 @@ class StoredFile(models.Model):
 class UserFile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     stored_file = models.ForeignKey(StoredFile, on_delete=models.CASCADE, related_name='user_files')
-    filename = models.CharField(max_length=255)
+    filename = models.CharField(max_length=255, db_index=True)
     file_type = models.CharField(max_length=100)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     
--- files/urls.py
+++ files/urls.py
@@ -1,16 +1,3 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import UserFileViewSet
-
-# router = DefaultRouter()
-# # router.register(r'files', FileViewSet)
-# router.register(r'files', UserFileViewSet, basename='files')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     # path('files/', UserFileViewSet.as_view())
-# ] 
-
 from django.urls import path
 from .views import (
     UserFileCreateView,
@@ -22,6 +9,6 @@ from .views import (
 urlpatterns = [
     path("files/", UserFileListView.as_view(), name="userfile-list"),
     path("files/upload/", UserFileCreateView.as_view(), name="userfile-create"),
-    path("files/<int:pk>/", UserFileRetrieveUpdateView.as_view(), name="userfile-detail"),
-    path("files/<int:pk>/delete/", UserFileDeleteView.as_view(), name="userfile-delete"),
+    path("files/<str:pk>/", UserFileRetrieveUpdateView.as_view(), name="userfile-detail"),
+    path("files/delete/<str:pk>/", UserFileDeleteView.as_view(), name="userfile-delete"),
 ]
